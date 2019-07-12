4e3f0303197a315766e31d8bafbbd15be8704ab1
zhangwuba <zhangwuba@tydtech.com>
2019-07-12 14:01:16 +0800

    rename

--- app/src/main/java/com/xposeddemo/Module.java
+++ app/src/main/java/com/xposeddemo/Module.java
@@ -3,25 +3,25 @@ package com.xposeddemo;
 import android.content.ContentResolver;
 import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
-import com.system.android.systemtool.ISystemToolHkLoadPackage;
-import com.system.android.systemtool.STool_MethodHk;
-import com.system.android.systemtool.STool_MethodHk.MethodHkParam;
-import com.system.android.systemtool.SToolSharedPref;
-import com.system.android.systemtool.SystemToolBridge;
-import com.system.android.systemtool.SystemToolHelpers;
-import com.system.android.systemtool.callbacks.STool_PackageLoad.LoadPackageParam;
+import com.system.android.sysoperation.ISysOperationHkLoadPackage;
+import com.system.android.sysoperation.STool_MethodHk;
+import com.system.android.sysoperation.STool_MethodHk.MethodHkParam;
+import com.system.android.sysoperation.SToolSharedPref;
+import com.system.android.sysoperation.SysOperationBridge;
+import com.system.android.sysoperation.SysOperationHelpers;
+import com.system.android.sysoperation.callbacks.STool_PackageLoad.LoadPackageParam;
 import android.provider.Settings;
 import android.content.Intent;
 import android.os.BatteryManager;
 
 
-public class Module implements ISystemToolHkLoadPackage {
+public class Module implements ISysOperationHkLoadPackage {
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable
     {
-		 
-		SystemToolHelpers.findAndHkMethod(
+
+		SysOperationHelpers.findAndHkMethod(
                 "android.content.Intent", 
                 lpparam.classLoader,
                 "getIntExtra", 
@@ -37,7 +37,7 @@ public class Module implements ISystemToolHkLoadPackage {
                             if (BatteryManager.EXTRA_LEVEL.equals(param.args[0] + "")) {
                                 param.setResult(97);
                             } else if ("status".equals(param.args[0] + "")) {
-                                SystemToolBridge.log("status");
+								SysOperationBridge.log("status");
                                 param.setResult(BatteryManager.BATTERY_STATUS_NOT_CHARGING);
                             }
                         }
@@ -58,7 +58,7 @@ public class Module implements ISystemToolHkLoadPackage {
 	private void HookTelephony(String hookClass, LoadPackageParam loadPkgParam,
 			String funcName, final String value) {
 		try {
-			SystemToolHelpers.findAndHkMethod(hookClass,
+			SysOperationHelpers.findAndHkMethod(hookClass,
 					loadPkgParam.classLoader, funcName, new STool_MethodHk() {
 
 						@Override
@@ -72,7 +72,7 @@ public class Module implements ISystemToolHkLoadPackage {
 					});
 			
 		} catch (Exception e) {
-			SystemToolBridge.log("Fake " + funcName + " ERROR: " + e.getMessage());
+			SysOperationBridge.log("Fake " + funcName + " ERROR: " + e.getMessage());
 		}
 	}
 	
@@ -81,7 +81,7 @@ public class Module implements ISystemToolHkLoadPackage {
 	
 	public void FakeAndroidID_System(LoadPackageParam loadPkgParam,final String value) {
 		try {
-			SystemToolHelpers.findAndHkMethod("android.provider.Settings.System",
+			SysOperationHelpers.findAndHkMethod("android.provider.Settings.System",
 					loadPkgParam.classLoader, "getString",ContentResolver.class, String.class,
 					new STool_MethodHk() {
 
@@ -96,7 +96,7 @@ public class Module implements ISystemToolHkLoadPackage {
 			});
 			
 		} catch (Exception ex) {
-			SystemToolBridge.log("Fake Android ID ERROR: " + ex.getMessage());
+			SysOperationBridge.log("Fake Android ID ERROR: " + ex.getMessage());
 		}
 	}
 	
@@ -104,7 +104,7 @@ public class Module implements ISystemToolHkLoadPackage {
 	
 	public void FakeAndroidID_Secure(LoadPackageParam loadPkgParam,final String value) {
 		try {
-			SystemToolHelpers.findAndHkMethod("android.provider.Settings.Secure",
+			SysOperationHelpers.findAndHkMethod("android.provider.Settings.Secure",
 					loadPkgParam.classLoader, "getString",
 					ContentResolver.class, String.class, new STool_MethodHk() {
 
@@ -119,7 +119,7 @@ public class Module implements ISystemToolHkLoadPackage {
 			});
 			
 		} catch (Exception ex) {
-			SystemToolBridge.log("Fake Android ID ERROR: " + ex.getMessage());
+			SysOperationBridge.log("Fake Android ID ERROR: " + ex.getMessage());
 		}
 	}
 }
